865789f6023485330abf7f675f8939cc1ed6161a
ffe <felix.ferber@dscsag.com>
2023-08-08 14:13:26 +0200

    bruh, use npm start for debugging

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,14 +1,38 @@
 //import util from "./util.js";
 //import {foo, imagine} from "./util";
 // import  { foo } from "./util";
-import * as utilGen from "./util";
+import * as Util from "./util";
 
 
 
 // console.log(util);
 // console.log(foo);
 // console.log(imagine)
+function utilOutput() {
+    console.log(Util.default);
+    console.log(Util.imagine);
+    console.log(Util.foo);
+}
+function greeting() {
+    console.log("Hello");
+}
 
-console.log(utilGen.default);
-console.log(utilGen.imagine);
-console.log(utilGen.foo);
+function greetUser(name="user", message="Hello!") {
+    console.log(name);
+    console.log(message);
+}
+
+utilOutput();
+greeting();
+greetUser("Max", "Hi");
+greetUser("Max");
+
+function transformToObjects(numberArray) {
+    let returnArr = [];
+
+    numberArray.map((item) => returnArr.push({val : item}));
+
+    return returnArr;
+}
+
+console.log(transformToObjects([1,2,3]));
